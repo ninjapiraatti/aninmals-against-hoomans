8f6342d37b4a76d7a6f9869fb64c48f2451949f4
ninjapiraatti <ninjapiraatti@DESKTOP-UGBSIUI.localdomain>
2024-07-02 14:14:09 +0300

    Add simple physics based player movement

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 2f;
+    private Rigidbody2D rb;
+    private Vector2 movementDirection;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    void FixedUpdate() {
+        rb.velocity = movementDirection * movementSpeed;
+    }
+}
