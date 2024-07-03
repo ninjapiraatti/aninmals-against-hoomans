7be965c6fc8ed74420c951265296f80e6dc1a7f9
ninjapiraatti <ninjapiraatti@DESKTOP-UGBSIUI.localdomain>
2024-07-03 19:16:47 +0300

    Implement the grid generation

--- Assets/GridManager.cs
+++ Assets/GridManager.cs
@@ -9,6 +9,12 @@ public class GridManager : MonoBehaviour
 
     [SerializeField] private Tile _tilePrefab;
 
+    [SerializeField] private Transform _cam;
+
+    void Start() {
+        GenerateGrid();
+    }
+
     void GenerateGrid() {
         for (int x = 0; x < _width; x++) {
             for (int y = 0; y < _height; y++) {
@@ -16,5 +22,7 @@ public class GridManager : MonoBehaviour
                 spawnedTile.name = $"Tile {x} {y}";
             }
         }
+
+        _cam.transform.position = new Vector3((float)_width/2 -0.5f, (float)_height/2 -0.5f, -10);
     }
 }
